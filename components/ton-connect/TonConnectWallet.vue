fd6373b5b580bcd40ae1f7b08a81d9a84db62b8d
Gerasimov Maxim <maskwes@yandex.ru>
2023-03-14 14:36:37 +0300

    добавлен ton connect

---
+++ components/ton-connect/TonConnectWallet.vue
@@ -0,0 +1,84 @@
+<script lang="ts" setup>
+import QrcodeVue from 'qrcode.vue'
+import {connector} from "~/utils/ton-connect";
+const walletList = ref([])
+getWalletsList().then((result)=>{
+  walletList.value=result
+  console.log(result)
+})
+
+const showConnectDialog = ref(false)
+const qrCodeDialog = ref(false)
+const snackbarAuth = ref(false)
+
+const linkToConnect = ref(null)
+function connectWallet(wallet){
+  linkToConnect.value=connectTonWallet(wallet)
+  console.log(linkToConnect.value)
+  if(linkToConnect.value){
+    qrCodeDialog.value=true
+  }
+}
+
+const userSession = ref({})
+connector.onStatusChange((walletInfo)=>{
+      userSession.value=walletInfo
+      showConnectDialog.value=false
+      qrCodeDialog.value=false
+    }
+);
+</script>
+
+<template>
+  <v-dialog width="auto" v-model="showConnectDialog">
+    <v-sheet color="black" class="overflow-hidden" max-width="400">
+      <v-toolbar
+          color="surface"
+          title="Select wallet"
+      ></v-toolbar>
+      <v-dialog width="auto" v-model="qrCodeDialog">
+        <v-sheet max-width="350" class="pa-4">
+          <v-row>
+            <v-col class="d-flex justify-center" cols="12">
+              <QrcodeVue size="300" v-if="linkToConnect" :value="linkToConnect"></QrcodeVue>
+            </v-col>
+<!--            <v-col class="d-flex justify-center" cols="12">
+              <a :href="linkToConnect" target="_blank" class="v-btn" >
+                Auth with link
+              </a>
+            </v-col>-->
+          </v-row>
+        </v-sheet>
+      </v-dialog>
+      <v-row class="pa-4">
+        <v-col cols="12" v-for="(wallet,key) in walletList" :key="key">
+          <v-sheet @click="connectWallet(wallet)" class="w-100 pa-4" :style="{
+            cursor: 'pointer'
+          }" variant="flat" color="surface">
+            <v-row align="center">
+              <v-col cols="2">
+                <v-avatar rounded="lg">
+                  <v-img
+                      :src="wallet.imageUrl"
+                      :alt="wallet.name"
+                  ></v-img>
+                </v-avatar>
+              </v-col>
+              <v-col>
+                <h2 class="text-h6 font-weight-bold">
+                  {{ wallet.name }}
+                </h2>
+              </v-col>
+            </v-row>
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </v-sheet>
+  </v-dialog>
+  <v-btn v-if="!userSession?.account" @click="showConnectDialog=true" variant="flat" color="primary">
+    connect wallet
+  </v-btn>
+  <v-btn @click="exitFromWallet()" variant="flat" v-else color="error">
+    disconect wallet: {{userSession.account.address.slice(0,7)}} ...
+  </v-btn>
+</template>
---
+++ utils/ton-connect.ts
@@ -0,0 +1,28 @@
+import TonConnect from '@tonconnect/sdk';
+
+const manifestTonConnectWallet = 'https://static.def.team/vpnxton/tonconnect-manifest.json'
+export const connector = new TonConnect({
+    manifestUrl: manifestTonConnectWallet
+});
+
+export function exitFromWallet(){
+    connector.disconnect();
+}
+
+
+connector.restoreConnection();
+
+export async function getWalletsList(){
+   return await connector.getWallets()
+}
+
+export function connectTonWallet(wallet){
+    if(wallet.embedded || wallet.injected){
+        connector.connect({jsBridgeKey: wallet.jsBridgeKey});
+    }else if(wallet.bridgeUrl){
+        return connector.connect({
+            universalLink: wallet.universalLink,
+            bridgeUrl: wallet.bridgeUrl
+        });
+    }
+}
